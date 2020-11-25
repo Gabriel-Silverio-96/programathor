a1f371a8218dfb9554372cb90fc746989ea8157f
Silvério <contato@gabrielsilver.com.br>
2020-11-25 19:15:40 -0300

    Criado página 404

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BtnSecondary } from '../../components/UI/Button';
+
+//Style
+import './style.css';
 
 export default function Page404() {
     return (
-        <h1>404</h1>
+        <main className="center-content">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 m-auto text-center">
+                        <div className="title-large">
+                            <h1>404</h1>
+                            <p>Página buscada, não foi encontrada</p>
+                            <Link to="/">
+                                <BtnSecondary name="Voltar a página inicial" />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
